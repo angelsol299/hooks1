234684e085b76a1010b9fd9618a5eb7df1960323
angelsol299 <angelsol299@hotmail.com>
2019-03-26 22:47:06 +0100

    adding remove todo working

--- src/App.js
+++ src/App.js
@@ -66,6 +66,12 @@ function App() {
     setTodos(newTodos);
   };
 
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
@@ -75,6 +81,7 @@ function App() {
             index={index}
             todo={todo}
             completeTodo={completeTodo}
+            removeTodo={removeTodo}
           />
         ))}
         <TodoForm addTodo={addTodo} />
